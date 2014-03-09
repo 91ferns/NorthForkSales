2bba8e706901e6d41668f86a2ffc592a653370ab
Stephen Parente <stephen@ingenuitydesign.com>
2014-03-08 23:13:51 -0500

    Added mailing lists, updated copy, fixed top, and got site ready to go.

---
+++ mailer.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = (isset($_POST['FullName']) && !empty($_POST['FullName'])) ? $_POST['FullName'] : false;
+$co = (isset($_POST['Company']) && !empty($_POST['Company'])) ? $_POST['Company'] : false;
+$message = (isset($_POST['Message']) && !empty($_POST['Message'])) ? $_POST['Message'] : false;
+$email = (isset($_POST['Email']) && !empty($_POST['Email'])) ? $_POST['Email'] : false;
+
+$headers = array(sprintf("Reply-to: %s", $email), "From: mailer@91ferns.com");
+
+mail( "jhamlett@northforksales.com", "New Message from ".$name, $message."\n\nCompany: ".$co, implode("\r\n", $headers ) );
+
+header("Location: /?msg=sent");exit;
